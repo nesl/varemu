70350ccac3225c97ced8b169c4cbe3ba5a36bf2b
Lucas Wanner <lfwanner@gmail.com>
2013-02-08 12:11:15 -0800

    Moving Stellaris software to its own Git project (VaRTOS)

--- stel/lm3s6965/driverlib/gcc-cm3/rit128x96x4.d
+++
@@ -1,4 +0,0 @@
-gcc-cm3/rit128x96x4.o: rit128x96x4.c ../inc/hw_ssi.h ../inc/hw_memmap.h \
- ../inc/hw_sysctl.h ../inc/hw_types.h ../driverlib/debug.h \
- ../driverlib/gpio.h ../driverlib/pin_map.h ../driverlib/ssi.h \
- ../driverlib/sysctl.h rit128x96x4.h
--- stel/start_qemu.sh
+++
@@ -1,1 +0,0 @@
-../qemu-linaro/arm-softmmu/qemu-system-arm -M lm3s6965evb -kernel $1 -singlestep -variability ../power_model_data/instance_01.txt --serial stdio
