cfdd9788c4a776d5eb150f531bdb9b0760e5f47f
Lucas Wanner <lfwanner@gmail.com>
2013-08-08 15:58:18 -0700

    Start with 45nm power model, nographic

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,3 +1,3 @@
-qemu-linaro/arm-softmmu/qemu-system-arm -M versatilepb -m 256M -kernel linux/linux-3.6.3/arch/arm/boot/zImage -initrd linux/buildroot-2012.11/output/images/rootfs.cpio.gz -append "root=/dev/ram rdinit=/sbin/init" -singlestep -variability power_model_data/instance_01.txt -net nic -net user    
+qemu-linaro/arm-softmmu/qemu-system-arm -M versatilepb -m 256M -kernel linux/linux-3.6.3/arch/arm/boot/zImage -initrd linux/buildroot-2012.11/output/images/rootfs.cpio.gz -append "root=/dev/ram rdinit=/sbin/init" -singlestep -variability power_model_data/45nm_nc.txt -net nic -net user  -nographic
 
 
