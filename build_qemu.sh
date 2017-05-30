47b6e6051a100f87917ef49a986f09128a487941
Lucas Wanner <lfwanner@gmail.com>
2017-05-30 18:24:11 -0300

    Extract QEMU codebase

--- build_qemu.sh
+++ build_qemu.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 
+tar xjf qemu-linaro-13-03.28.tar.bz2
 cp -r varemu/* qemu-linaro/
 cd qemu-linaro
 ./configure --disable-kvm --enable-debug --enable-debug-tcg --target-list=arm-softmmu --audio-drv-list=alsa
